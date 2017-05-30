159a85bcdb823bb175118d1ffd4be88fa379613d
cabri89 <cabridens@gmail.com>
2017-05-31 01:11:52 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const mysql          = require('mysql');
+const app            = express();
+const db             = require('./config/db');
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var con = mysql.createConnection(db);
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  require('./app/routes')(app,con, {});
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+});
