bb7c1c7aa1e513eb5d86e99e92f5ba830cee07cc
cabri89 <cabridens@gmail.com>
2017-05-31 19:24:33 +0200

    same

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,6 @@
 // routes/note_routes.js
 module.exports = function(app, con) {
-    app.post('/car/:uid/:useruid', (req, res) => {
+    app.post('/car/:uid/:useruid', function (req, res) {
         const uid = req.params.uid;
         const userUid = req.params.useruid;
 
